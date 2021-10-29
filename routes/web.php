fd22c5328b38338a2dc237aeaf6519a3dad85064
mutiara-raudhatul <1911521011_mutiara@student.unand.ac.id>
2021-10-29 19:04:35 +0700

    akun anggota

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DivisiController;
+use App\Http\Controllers\AkunController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,8 @@ Route::get('/', function () {
 
 Route::get('/divisi', [DivisiController::class, 'index'])->name('divisi');
 Route::get('/create-divisi', [DivisiController::class, 'create'])->name('create-kelas');
+
+Route::get('/list-akun', [AkunController::class, 'index'])->name('list-akun');
+Route::get('/list-akun/{id}/detail1', [AkunController::class, 'show1'])->name('detail-akun1');
+Route::get('/list-akun/{id}/detail2', [AkunController::class, 'show2'])->name('detail-akun2');
+Route::get('/create-akun', [AkunController::class, 'create'])->name('create-akun');
